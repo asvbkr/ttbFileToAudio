a19cb3a9db4d164aa823320da37b4dd673e9b922
Agaltsov Sergey <sagalcov@roskazna.ru>
2019-08-29 12:19:28 +0300

    Убрано дублирование кода требуемых библиотек

--- TamTamBot/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: UTF-8 -*-
-from TamTamBot import TamTamBot
-from .cls.ChatExt import ChatExt
-from .cls.CallbackButtonCmd import CallbackButtonCmd
-from .cls.UpdateCmn import UpdateCmn
