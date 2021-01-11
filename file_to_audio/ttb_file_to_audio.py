758f32768f442f1b5d1a318e287a886f8675d623
asvbkr <asv@bk.ru>
2021-01-11 13:16:14 +0300

    Реализация бота

--- file_to_audio/ttb_file_to_audio.py
+++ file_to_audio/ttb_file_to_audio.py
@@ -1,10 +1,12 @@
 # -*- coding: UTF-8 -*-
 import os
 
+import requests
+
 from TamTamBot import CallbackButtonCmd, UpdateCmn, ChatExt
 from TamTamBot.utils.lng import get_text as _
 from TamTamBotDj.TamTamBotDj import TamTamBotDj
-from openapi_client import BotCommand, Intent, ChatType, ChatAdminPermission
+from openapi_client import BotCommand, Intent, ChatType, ChatAdminPermission, LinkButton, FileAttachment, UploadType, NewMessageLink, MessageLinkType, NewMessageBody, SimpleQueryResult
 
 
 class TtbFileToAudio(TamTamBotDj):
@@ -17,20 +19,28 @@ class TtbFileToAudio(TamTamBotDj):
     @property
     def description(self):
         # type: () -> str
-        return 'Этот и не бот, в общем-то, а заготовка для него.\n\n' \
-               'This is not a bot, in general, but a blank for him.'
+        return 'Этот бот создаёт (при возможности) сообщения с типом "аудио" на основе новых файлов, прикрепляемых в ваших чатах. ' \
+               'Вам нужно добавить бота в качестве администратора с разрешениями «Читать сообщения» и "Писать, редактировать и удалять сообщения".\n' \
+               'Чтобы открыть меню, введите /menu.\n\n' \
+               'This bot creates, if possible, messages of type "audio" based on new files attached to your chats. ' \
+               'You need to add the bot to it as an administrator with the "Read all messages" and "Write, edit and delete messages" permissions.\n' \
+               'To open the menu, type /menu.'
+
+    @property
+    def about(self):
+        # type: () -> str
+        return self.description
 
     def get_commands(self):
         # type: () -> [BotCommand]
-        self.lgz.warning('The default command list is used. Maybe is error?')
         commands = [
             BotCommand('start', 'начать (о боте) | start (about bot)'),
             BotCommand('menu', 'показать меню | display menu'),
-            BotCommand('list_all_chats', 'список всех чатов | list all chats'),
-            BotCommand('view_chats_available', 'доступные чаты | available chats'),
-            BotCommand('subscriptions_mng', 'управление подписками | managing subscriptions'),
-            BotCommand('view_chats_attached', 'подключенные чаты | attached chats'),
-            BotCommand('view_buttons_test', 'тест кнопок | buttons test'),
+            # BotCommand('list_all_chats', 'список всех чатов | list all chats'),
+            # BotCommand('view_chats_available', 'доступные чаты | available chats'),
+            # BotCommand('subscriptions_mng', 'управление подписками | managing subscriptions'),
+            # BotCommand('view_chats_attached', 'подключенные чаты | attached chats'),
+            # BotCommand('view_buttons_test', 'тест кнопок | buttons test'),
 
         ]
         if len(self.languages_dict) > 1:
@@ -40,14 +50,9 @@ class TtbFileToAudio(TamTamBotDj):
     @property
     def main_menu_buttons(self):
         # type: () -> []
-        self.lgz.warning('The default main menu buttons is used. Maybe is error?')
         buttons = [
-            [CallbackButtonCmd(_('About bot'), 'start', intent=Intent.POSITIVE, bot_username=self.username)],
-            [CallbackButtonCmd(_('All chat bots'), 'list_all_chats', intent=Intent.POSITIVE, bot_username=self.username)],
-            [CallbackButtonCmd('Доступные чаты | Available chats', 'view_chats_available', intent=Intent.POSITIVE, bot_username=self.username)],
-            [CallbackButtonCmd('Управление подписками | Managing subscriptions', 'subscriptions_mng', intent=Intent.POSITIVE, bot_username=self.username)],
-            [CallbackButtonCmd('Подключенные чаты | Attached chats', 'view_chats_attached', intent=Intent.POSITIVE, bot_username=self.username)],
-            [CallbackButtonCmd('Тест кнопок | Buttons test', 'view_buttons_test', intent=Intent.POSITIVE, bot_username=self.username)],
+            [CallbackButtonCmd(_('About bot'), 'start', intent=Intent.POSITIVE)],
+            [LinkButton(_('Chat to discuss the bot'), 'https://tt.me/asvbkr_sup')],
         ]
         if len(self.languages_dict) > 1:
             buttons.append([CallbackButtonCmd('Изменить язык / set language', 'set_language', intent=Intent.DEFAULT, bot_username=self.username)])
@@ -61,7 +66,7 @@ class TtbFileToAudio(TamTamBotDj):
             if user_id:
                 pass
             ap = chat_ext.admin_permissions.get(self.user_id)
-            return ap and ChatAdminPermission.WRITE in ap
+            return ap and ChatAdminPermission.WRITE in ap and ChatAdminPermission.READ_ALL_MESSAGES in ap
 
     def cmd_handler_view_chats_available(self, update):
         # type: (UpdateCmn) -> bool
@@ -113,3 +118,47 @@ class TtbFileToAudio(TamTamBotDj):
                 lt[-1].append(CallbackButtonCmd(s, s, intent=Intent.DEFAULT, bot_username=self.username))
 
         self.view_buttons('Test', lt, user_id=update.user_id, add_close_button=True, add_info=True)
+
+    def receive_message(self, update):  # type: (UpdateCmn) -> bool
+        create_link = False
+        delete_source = True
+        if update and update.message and update.message.body:
+            update.message.body.attachments = update.message.body.attachments or []
+            ats = []
+            names = []
+
+            for attachment in update.message.body.attachments:
+                if isinstance(attachment, FileAttachment):
+                    self.lgz.debug('(%s).' % attachment)
+                    url = attachment.payload.url
+                    name = attachment.filename
+                    if name:
+                        name_l = name.split('.')
+                        if len(name_l) > 1:
+                            name = '.'.join(name_l[:-1])
+                    names.append(name)
+                    r = requests.get(url)
+                    if r.status_code == 200:
+                        ats.extend(self.attach_contents([(r.content, UploadType.AUDIO)]))
+            if ats:
+                if create_link:
+                    link = NewMessageLink(MessageLinkType.REPLY, update.message.body.mid)
+                else:
+                    link = None
+                mb = NewMessageBody(attachments=ats, link=link)
+                mb.text = '; '.join(names)
+                try:
+                    self.send_message(mb, chat_id=update.chat_id)
+                except Exception as e:
+                    self.lgz.warning(e)
+                else:
+                    self.lgz.debug(f'it\'s all right!')
+                    if delete_source:
+                        self.lgz.debug(f'try delete source with id={update.message.body.mid}')
+                        try:
+                            res_d = self.msg.delete_message(update.message.body.mid)
+                            if isinstance(res_d, SimpleQueryResult):
+                                self.lgz.debug(f'result delete source with id={update.message.body.mid}: {res_d.success}-{res_d.message}')
+                        except Exception as e:
+                            self.lgz.warning(e)
+        return True
